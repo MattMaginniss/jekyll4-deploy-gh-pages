3201f0b2234d5c083110ee63e7fb92a88dfef589
Matt Maginniss <maginniss.matt@gmail.com>
2023-02-06 21:22:33 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,19 @@ REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 BRANCH="gh-pages"
 BUNDLE_BUILD__SASSC=--disable-march-tune-native
 
-echo "Installing gems..."
+# echo "Installing gems..."
 
-bundle config path vendor/bundle
-bundle install --jobs 4 --retry 3
+# bundle config path vendor/bundle
+# bundle install --jobs 4 --retry 3
 
-echo "Building Jekyll site..."
+# echo "Building Jekyll site..."
 
-if [ ! -z $YARN_ENV ]; then
-  echo "Installing javascript packages..."
-  yarn
-fi
+# if [ ! -z $YARN_ENV ]; then
+#   echo "Installing javascript packages..."
+#   yarn
+# fi
 
-JEKYLL_ENV=production NODE_ENV=production bundle exec jekyll build
+# JEKYLL_ENV=production NODE_ENV=production bundle exec jekyll build
 
 echo "Publishing..."
 
